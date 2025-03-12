7cd914c6a881388279ca8b2bfea7ccd5131f9316
Andrew Sansum <sansumandrew@gmail.com>
2025-03-12 13:28:03 +0000

    Complete problem 3

--- src/Problem3.hs
+++ src/Problem3.hs
@@ -1,4 +1,6 @@
 module Problem3 (elementAt) where
 
 elementAt :: [a] -> Int -> Maybe a
-elementAt = undefined
+elementAt [] _ = Nothing
+elementAt (x : _) 0 = Just x
+elementAt (_ : xs) i = elementAt xs (i - 1)
